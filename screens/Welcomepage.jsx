f2876fc95e53ed16c0a6444b2d27e607dc3db0ca
Nagaraj K I <nagarajki@ganakalabs.com>
2023-03-13 10:56:03 +0530

    first commit

---
+++ screens/Welcomepage.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Welcomepage = () => {
+  return (
+    <View>
+      <Text>Welcomepage</Text>
+    </View>
+  )
+}
+
+export default Welcomepage
+
+const styles = StyleSheet.create({})
